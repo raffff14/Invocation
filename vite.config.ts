e3a389d036b36f1159c44b5459de95db30ceeb3a
raffff14 <132864451+raffff14@users.noreply.github.com>
2024-10-24 03:45:30 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [
     react(),
-    {
-      name: 'api',
-      configureServer(server) {
-        server.middlewares.use(async (req, res, next) => {
-          if (req.url?.startsWith('/api/')) {
-            const { viteNodeApp } = await server.ssrLoadModule('./src/api/server.ts');
-            viteNodeApp(req, res, next);
-          } else {
-            next();
-          }
-        });
-      },
-    },
+   
   ],
 });
